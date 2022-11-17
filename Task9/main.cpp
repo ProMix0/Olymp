433f3a9fe04e9fcd04a73237cee596e0cdf371e3
Anton Maltsev <nokiaru9999@gmail.com>
2022-11-17 13:00:24 +0700

    Fix task 9

--- Task9/main.cpp
+++ Task9/main.cpp
@@ -4,21 +4,18 @@
 
 int main()
 {
-	unsigned long a, b;
+	uint64_t a, b;
 	std::cin >> a >> b;
 	if (a == b)
 	{
 		std::cout << 0 << std::endl;
 		return 0;
 	}
-	if (a == 0)
-	{
-		std::cout << -1 << std::endl;
-		return 0;
-	}
 	std::string astr = std::bitset<64>(a).to_string(), bstr = std::bitset<64>(b).to_string();
-	astr = astr.substr(astr.find('1'));
-	bstr = bstr.substr(bstr.find('1'));
+	auto aptr = astr.find('1');
+	auto bptr = bstr.find('1');
+	astr = astr.substr(aptr == std::string::npos ? 63 : aptr);
+	bstr = bstr.substr(bptr == std::string::npos ? 63 : bptr);
 	int indx = 0;
 	if ((indx = astr.find(bstr)) != std::string::npos)
 		std::cout << indx << std::endl;
